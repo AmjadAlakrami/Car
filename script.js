871044dec684456c251ea7645d53e56acac2f6b4
AmjadAlakrami <amjad.alakrmi@abbbindustrigymnasium.se>
2019-12-22 02:13:25 +0100

    Script kod komentering

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+//  Anslutning funktion 
+// fuktionen körs när man trycker på knappen som har "my-super-cool-btn" id
 function startConnect() {
+  // if-satsen kollar om vi är connectade 
   if (document.getElementById("my-super-cool-btn").innerHTML == "Connect"){
     document.getElementById("my-super-cool-btn").innerHTML = "Disconnect";
+    //Ändrar CSS:en på knappen 
     document.getElementById("test").className = "my-super-cool-btn2";
-      // Generate a random client ID
+      // Genererar en random client ID
     clientID = "clientID_" + parseInt(Math.random() * 100);
-      // Fetch the hostname/IP address and port number from the form
+      // Hämta hostname/ IP-adress och portnummer
     host ='maqiatto.com';
     port = 8883;
-      // Print output for the user in the messages div
     document.getElementById("messages").innerHTML = 'Connected'
 
-    // Initialize new Paho client connection
+    // ny Paho-klientanslutning
     client = new Paho.MQTT.Client(host, Number(port), clientID);
-      // Set callback handlers
+      // Ställ in återuppringningshanterare
     client.onConnectionLost = onConnectionLost;
     client.onMessageArrived = onMessageArrived;
     client.connect({userName : "amjad.alakrami@abbindustrigymnasium.se",password : "amjad1314152",
@@ -27,19 +30,17 @@ function startConnect() {
     document.getElementById("messages").innerHTML = 'Disconnected';
   }
 }
-
+//Om anslutningen misslyckas 
 function onFail() {
   document.getElementById("messages").innerHTML = 'ERROR!'
 }  
-
+// två topic som tar emot meddelanden och en som skickar 
 let topic_1 ="amjad.alakrami@abbindustrigymnasium.se/car";
 let topic_2 ="amjad.alakrami@abbindustrigymnasium.se/speed";
 let topic_3 ="amjad.alakrami@abbindustrigymnasium.se/speed1"
-// Called when the client connects
+// Om anslutningen lyckas 
 function onConnect() {
-   // Fetch the MQTT topic from the form
-  console.log(topic_1);
-   // Subscribe to the requested topic
+   // Subscribe till alla topics
     client.subscribe(topic_1);
     client.subscribe(topic_2);
     client.subscribe(topic_3);
@@ -47,10 +48,12 @@ function onConnect() {
     message.destinationName=topic_1;
     client.send(message);
 }
-
+// En funktion som skickar de meddelanden vi vill skicka
 function onSend() {
   var input = document.getElementById("newMessage");
+  // En fuktion som kollar om ett knapptryck på input rutan med idet "newMessage"
   input.onkeyup = function(event) {
+  // if-satsen checkar om det var ENTER knappen "13 = enter"
   if (event.keyCode === 13) {
     event.preventDefault();
     document.getElementById("f").innerHTML = document.getElementById("newMessage").value;
@@ -63,11 +66,8 @@ function onSend() {
   }
   }
 }
-    // Fetch the MQTT topic from the form
-    // Print output for the user in the messages div
 
-
-// Called when the client loses its connection
+// Om anslutningen förloras 
 function onConnectionLost(responseObject) {
     document.getElementById("messages").innerHTML = 'ERROR!';
     if (responseObject.errorCode !== 0) {
@@ -75,16 +75,14 @@ function onConnectionLost(responseObject) {
     }
 }
 
-// Called when a message arrives
+// En function som körs när vi får ett meddelande 
 function onMessageArrived(message) {
-
-  console.log("onMessageArrived: " + message.payloadString);
+  // if-satsen checkar om meddelandet vi får är en dictionary i string form
   if (message.payloadString[0]=="{") {
-    
+  // gör om det till en JSON-object
   var info = JSON.parse(message.payloadString);
-  
   }
-  
+  //till vilken topic är meddelandet skickad 
   if (message.destinationName == topic_2){
     document.getElementById("PWMD").innerHTML = "David PWM" + "<br>" + info.pwm;
     adddata(info.speedo, 0);
@@ -94,15 +92,13 @@ function onMessageArrived(message) {
     adddata(info.speedo,  2);
   }
 }
-// Called when the disconnection button is pressed
-
-
+// En fuktion som körs efter nya inputs från text rutan eller slider
 function onInput (){
     var slider = document.getElementById("myRange");
     var output = document.getElementById("f");
     output.innerHTML = slider.value; // Display the default slider value
 
-    // Update the current slider value (each time you drag the slider handle)
+    // Uppdatera det aktuella slider värdet varje gången man drar den 
     slider.onmouseup  = function() {
     output.innerHTML = this.value;
     
@@ -115,34 +111,39 @@ function onInput (){
 
     }
 }
-
+// En fucktion som lägger till data till grafen 
+// första argumentet är de värdena vi lägga till grafen, och den andra är till vilken dictionary i grafen dataset (Amjad, david eller börvärdet)
 function adddata(data, index){
-  var d = new Date();
-  var n = d.getSeconds();
+  var date = new Date();
+  var sec = date.getSeconds();
+  // om grapens data är större än 10
   if (myLineChart.data.labels.length > 10){
+  // shift tar bort det första värdet i listan 
   myLineChart.data.labels.shift();
   myLineChart.data.datasets[index].data.shift();
   myLineChart.data.datasets[1].data.shift();
+  // push lägger till data till listan 
   myLineChart.data.datasets[index].data.push(data);
   myLineChart.data.datasets[1].data.push( document.getElementById("myRange").value);
-  myLineChart.data.labels.push(n);
+  myLineChart.data.labels.push(sec);
   myLineChart.update();
   }
   else{
     myLineChart.data.datasets[index].data.push(data);
     myLineChart.data.datasets[1].data.push( document.getElementById("myRange").value);
-    myLineChart.data.labels.push(n);
+    myLineChart.data.labels.push(sec);
     myLineChart.update();
   }
 }
 
 
 var myLineChart = null;
-
+// En fucktion som skapar grafen 
 function chart(){
   var canvas = document.getElementById('Chart');
   var data = {
   labels: [],
+  // varje dictionary i dataset är en line som visualiseras i grafen 
   datasets: [
       {
           label: "Ärvärdet David",
@@ -207,7 +208,6 @@ stroke: {
 },
 series: [{
     data: data,
-    // data1: data1
 }],
 markers: {
     size: 0
